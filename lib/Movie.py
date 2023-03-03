797534cc9af7737bbedebb903bf1ecc6fd05a5ed
Julian Patterson <julian.patterson@gmail.com>
2023-03-03 12:16:08 -0500

    Submitted

--- lib/Movie.py
+++ lib/Movie.py
@@ -1,13 +1,44 @@
+# import sys
+
+# sys.setrecursionlimit(1500)
+
 class Movie:
     
     def __init__(self, title):
-        pass
+        self.title = title
+        self.reviews = []
+        self.reviewers = []
 
-    # title property goes here!
+    def get_title(self):
+        print("Retrieving title:")
+        return self._title
+
+    # @title.setter
+    def set_title(self, title):
+        if type(title) == str and 0 < len(title):
+            self._title = title
+        else:
+            print("The movie title should be at least 1 character")
+            raise Exception("The movie title should be at least 1 character")
+        
+    title = property(get_title, set_title)
+        
+    def get_reviewers(self):   
+        return self.reviewers
+    
+    def reviewers(self, reviewers=[]):
+        if type(reviewers) == []:
+            self.reviewers = reviewers
 
     def average_rating(self):
-        pass
+        return sum(self.reviews) / len(self.reviews)
 
-    @classmethod
-    def highest_rated(cls):
-        pass
+    # @classmethod
+    # def highest_rated(cls):
+
+    #     highest_rated = 0
+    #     for review in reviews:
+    #         if review >= highest_rated:
+    #             highest_rated = review
+    #             return highest_rated.title
+
--- lib/Review.py
+++ lib/Review.py
@@ -1,10 +1,39 @@
 class Review:
     
-    def __init__(self, viewer, movie, rating):
-        pass
+    def __init__(self, viewer, movie, rating, username):
+        self.viewer = viewer
+        self.movie = movie
+        self.rating = rating
+        self.username = username
 
     # rating property goes here!
+    def get_rating(self):
+        print("Retrieving rating:")
+        return self._rating
+
+    # @title.setter
+    def set_rating(self, rating):
+        if type(rating) == int and 1 <= rating <= 5 :
+            self._rating = rating
+        else:
+            print("The rating must be between 0 and 5")
+            raise Exception("The rating must be between 0 and 5")
+        
+    rating = property(get_rating, set_rating)        
 
     # viewer property goes here!
+    def get_username(self):
+        print("Retrieving username:")
+        return self._username
+
+    def set_username(self, username):
+        if hasattr(username, username == str):
+            self.username = username
+        else:
+            print("Username must be a unique")
+            raise Exception("Username must be a unique")
+        
+    username = property(get_username, set_username)     
+
 
     # movie property goes here!
